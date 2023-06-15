ce7295a2852397994f4e59e551dabed6caef2426
Ayushi_9 <92251474+Ayushi912@users.noreply.github.com>
2023-06-15 11:16:46 +0530

    Create SingleElementInSortedArray.cpp

---
+++ SingleElementInSortedArray.cpp
@@ -0,0 +1,15 @@
+int singleNonDuplicate(vector<int>& nums)
+{
+	// Write your code here
+	int low = 0, high = nums.size() - 2; 
+        while(low <= high) {
+            int mid = (low + high) >> 1; 
+            if(nums[mid] == nums[mid^1]) {
+                low = mid + 1; 
+            } 
+            else {
+                high = mid - 1; 
+            }
+        }
+        return nums[low]; 
+}
