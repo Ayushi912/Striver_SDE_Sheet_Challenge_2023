ff16c1aa9876b2728556ab8c8fc0e1781b0564a8
Ayushi_9 <92251474+Ayushi912@users.noreply.github.com>
2023-06-15 11:18:19 +0530

    Create SearchInRotatedSortedArray.cpp

---
+++ SearchInRotatedSortedArray.cpp
@@ -0,0 +1,27 @@
+int search(int* a, int n, int target) {
+    // Write your code here.
+    int low = 0, high = n - 1; 
+        while(low <= high) {
+            int mid = (low + high) >> 1; 
+            if(a[mid] == target) return mid; 
+            
+            // the left side is sorted
+            if(a[low] <= a[mid]) {
+                if(target >= a[low] && target <= a[mid]) {
+                    high = mid - 1; 
+                }
+                else {
+                    low = mid + 1; 
+                }
+            }
+            else {
+                if(target >= a[mid] && target <= a[high]) {
+                    low = mid + 1; 
+                }
+                else {
+                    high = mid - 1; 
+                }
+            }
+        } 
+        return -1; 
+}
