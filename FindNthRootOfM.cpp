e5481f0a1b0d2251470fb1580539457d38a33fe7
Ayushi_9 <92251474+Ayushi912@users.noreply.github.com>
2023-06-26 14:52:55 +0530

    Update FindNthRootOfM.cpp

--- FindNthRootOfM.cpp
+++ FindNthRootOfM.cpp
@@ -1,47 +1,20 @@
-bool power(long long a, long long b, int num)
-{
-  long long res = 1;
-  while(b)
-  {
-    if(a > num)
-      return false;
-    if(b & 1)
-      res *= a;
-    if(res > num)
-      return false;
-    a *= a;
-    b >>= 1;
-  }
-  return true;
-}
-
-int powerr(int a, int b)
-{
-  int res = 1;
-  while(b)
-  {
-    if(b & 1)
-      res *= a;
-    a *= a;
-    b >>= 1;
-  }
-  return res;
-}
-
 int NthRoot(int n, int m) {
   // Write your code here.
-  int l = 1, r = m + 1;
-  while(r - l > 1)
-  {
-    int mid = l + (r - l) / 2;
-    // cout << mid << ' ' << power(mid, n, m) << endl;
-    if(power(mid, n, m))
-      l = mid;
-    else
-      r = mid;
-  }
-  // cout << "L -> " << l << endl;
-  if(power(l, n, m) && powerr(l, n) == m)
-    return l;
-  return -1;
+    int i=0;
+    int j=m+1;
+    while(i+1<j){
+        int a=(i+j)/2;
+        if(pow(a,n)>m){
+        j=a;
+        }
+        else{
+        i=a;
+        }
+    }
+    if(pow(i,n)==m){
+        return i;
+    }
+    else{
+        return -1;
+    }
 }
