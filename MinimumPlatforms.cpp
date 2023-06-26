5b1bbd81f5ab8fbf0c638795533b8bf7b6f8c8ff
Ayushi_9 <92251474+Ayushi912@users.noreply.github.com>
2023-06-26 12:38:19 +0530

    Create MinimumPlatforms.cpp

---
+++ MinimumPlatforms.cpp
@@ -0,0 +1,24 @@
+int calculateMinPatforms(int at[], int dt[], int n) {
+    // Write your code here.
+    sort(at,at+n);
+    sort(dt,dt+n);
+ 
+    int ans=1;
+    int count=1;
+    int i=1,j=0;
+    while(i<n && j<n)
+    {
+        if(at[i]<=dt[j]) //one more platform needed
+        {
+            count++;
+            i++;
+        }
+        else //one platform can be reduced
+        {
+            count--;
+            j++;
+        }
+        ans=max(ans,count); //updating the value with the current maximum
+    }
+    return ans;
+}
