75e4ef048af927688b938fee06e981a400f354e2
Ayushi_9 <92251474+Ayushi912@users.noreply.github.com>
2023-06-26 18:46:54 +0530

    Create TopologicalSort.cpp

---
+++ TopologicalSort.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    // Write your code here
+    //make adj list 
+    unordered_map<int,list<int>> adj;
+    for(int i = 0;i<e;i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        
+        adj[u].push_back(v);
+    }
+    
+    //find all indegree
+    vector<int> indegree(v);
+    for(auto i:adj){
+        for(auto j:i.second){
+            indegree[j]++;
+        }
+    }
+    //0 indegree wale ko push kr do
+    queue<int> q;
+    for(int i = 0;i<v;i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+    
+    //do bfs
+    vector<int> ans;
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+        ans.push_back(front);
+        
+        //neighbour indegree ko update kr do
+        for(auto i:adj[front]){
+            indegree[i]--;
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        
+    }
+    return ans;
+}
